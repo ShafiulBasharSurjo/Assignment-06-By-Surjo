149825d62c2b99b1e2a2c4d5255704ba611ee150
ShafiulBasharSurjo <shafiulbasharprodhan@gmail.com>
2026-04-01 16:34:35 +0600

    Last commit with minor fixes and dist file

--- src/Componenets/Card/Card.jsx
+++ src/Componenets/Card/Card.jsx
@@ -56,6 +56,20 @@ const Card = ({ dataPromise, setCartItems }) => {
           <div className="mt-6" onClick={() => setSelectedItems(true)}>
             {selectedItems === true ? (
               <button className="btn btn-success btn-block rounded-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-white-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
                 Added to Cart
               </button>
             ) : (
